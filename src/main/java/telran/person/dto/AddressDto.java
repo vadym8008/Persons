cbaf00e343c28907d3a1260f87c62f23a627def9
vadim <ivashchenko8008@gmail.com>
2018-08-27 20:14:08 +0300

    Initial commit

---
+++ src/main/java/telran/person/dto/AddressDto.java
@@ -0,0 +1,19 @@
+package telran.person.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class AddressDto {
+	String city;
+	String street;
+	int building;
+
+}
